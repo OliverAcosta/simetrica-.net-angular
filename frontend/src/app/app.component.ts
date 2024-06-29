d035ad98dc5fd928b4f62a8611a649de633584a0
Juan Acosta <acosta.herrera.oliver@gmail.com>
2024-06-29 15:41:32 -0400

    adding layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list'
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    RouterLink,
+    LoginComponent,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule,
+    MatExpansionModule,
+    MatMenuModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
